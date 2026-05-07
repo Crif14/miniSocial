f0365878fa963b948cf5bdb36364928a24058313
Crif14 <CSqueo22@gmail.com>
2026-05-07 17:34:15 +0200

    Update Topic e post

--- echo-social/app/Models/DailyTopic.php
+++ echo-social/app/Models/DailyTopic.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class DailyTopic extends Model
 {
-    //
+    protected $table = 'dailyTopics';
+
+    protected $fillable = ['userId', 'title', 'description', 'topicDate'];
+
+    protected $casts = [
+        'topicDate' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function scopeToday($query)
+    {
+        return $query->whereDate('topicDate', Carbon::today());
+    }
 }
--- echo-social/app/Models/PostLike.php
+++ echo-social/app/Models/PostLike.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostLike extends Model
 {
-    //
+    protected $table = 'postLikes';
+
+    public $timestamps = false;
+
+    protected $fillable = ['userId', 'postId'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'postId');
+    }
 }
