1010027e5af541adce9c5e3aca3cba6665bd7eea
Crif14 <CSqueo22@gmail.com>
2026-05-07 17:32:11 +0200

    Update Comment

--- echo-social/app/Models/Comment.php
+++ echo-social/app/Models/Comment.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+
+    protected $fillable = ['postId', 'userId', 'body', 'isFlagged'];
+
+    protected $casts = [
+        'isFlagged' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'postId');
+    }
 }
