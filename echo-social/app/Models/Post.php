b3abb8c4372bf77f328ca61886dcff2ef7dd3d92
Crif14 <CSqueo22@gmail.com>
2026-05-07 17:31:31 +0200

    Update Post

--- echo-social/app/Models/Post.php
+++ echo-social/app/Models/Post.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+
+    protected $fillable = ['userId', 'body', 'isFlagged', 'flaggedReason'];
+
+    protected $casts = [
+        'isFlagged' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'postId')->latest();
+    }
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'postLikes', 'postId', 'userId');
+    }
+
+    public function embedding()
+    {
+        return $this->hasOne(Embedding::class, 'postId');
+    }
+
+    public function isLikedBy($user)
+    {
+        return $this->likes()->where('userId', $user->id)->exists();
+    }
 }
